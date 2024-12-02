00283e557c5e01cb070c52a2fe135548cccee0ef
Alexandra Cuaycal <acuaycalbastidas@ufl.edu>
2024-12-02 17:23:40 -0500

    Update Genes_of_interest_expression.R

--- Genes_of_interest_expression.R
+++ Genes_of_interest_expression.R
@@ -29,23 +29,10 @@ INS_pos_CD3_neg <- islet_id[islet_id$Histological_phenotype == "INS+CD3-",]
 #subset gene expression for INS+ CD3- islets
 gene_expression_subset_NI <- gene_expression[,colnames(gene_expression) %in% INS_pos_CD3_neg$Sample]
 
-# to get data only from INS + CD3+
-INS_pos_CD3_pos <- islet_id[islet_id$Histological_phenotype == "INS+CD3+",]
-
-## removing 1 INS + CD3+ islet from ND donors
-INS_pos_CD3_pos <- subset(INS_pos_CD3_pos, Clinical_phenotype != "ND")
-
-#subset gene expression for INS+ CD3+ islets
-gene_expression_subset_I <- gene_expression[,colnames(gene_expression) %in% INS_pos_CD3_pos$Sample]
-
-
 ## for convenience, rewrite gene_expression with gene_expression_subset
-
 #Not insulitic (CD3-)
 gene_expression_NI <- gene_expression_subset_NI
 
-#Insulitic (CD3+)
-gene_expression_I <- gene_expression_subset_I
 
 ################ Load genes of interest #########################
 ## Huber et al. marker genes
@@ -53,7 +40,6 @@ genes <- read.csv("Huber et al - target genes.csv")
 
 # get gene description data for genes of interest
 genes_list <- gene_description_data[trimws(gene_description_data$genesymbol) %in% genes$genesymbol,]
-genes_list <- subset(genes_list, locus.type == "Coding")
 
 ## custom function to calculate average, sd and se of expression values
 ## use expression_values = T to get expression values for all islets
@@ -62,25 +48,14 @@ genes_list <- subset(genes_list, locus.type == "Coding")
 list_expression_NI <- table_ready(gene_expression_NI, genes_list)
 write.csv(list_expression_NI, file = "Gene expression avg.csv", row.names = TRUE)
 
-# anova followed by tukey test (islet-based comparisons)
-tukey_list_NI <- tukey_tests(gene_expression_NI, genes_list)$tukey_list
-write.csv(Tukey_list_NI, file="Tukey tests - genes of interest.csv", row.names = F)
+############# Donor average comparisons for Huber et al. paper ######################
 
-
-# Create gene expression plots for each gene of interest (islet-based comparisons)
-# uses custom function and adds statistical significance based on anova follow by tukey-test (plots comparisons to ND group only)
-gene_expression_graphs(gene_expression = gene_expression_NI, 
-                       gene_description = gene_description_data, 
-                       genes_interest = genes)
-
-############# Donor average comparisons ######################
 # Get expression matrix for genes of interest
 genes_expression_values <- table_ready(gene_expression_NI, genes_list, expression_values = T)[[1]]
-genes_expression_values <- as.data.frame(genes_expression_values)
 genes_expression_values$probe_id <- rownames(genes_expression_values)
 
 # Create long expression table
-Avg_expression_donors <- pivot_longer(genes_expression_values, values_to = "Expression", names_to = "Sample", cols = 1:78)
+Avg_expression_donors <- melt(Mollie_genes_expression_values[,!colnames(Mollie_genes_expression_values) == ""]  , value.name = "Expression" , variable.name='Sample')
 
 # add donor metdata
 Avg_expression_donors <- merge(Avg_expression_donors, INS_pos_CD3_neg[,c(1,2,5,6,21)])
